ecc03e6f65c24316d92bcc28214d93e6e67f4164
Mateo Fidabel <mateofidabel@gmail.com>
2021-09-23 22:20:16 -0400

    Se agrega benchs de Minimax y Alfabeta

--- src/componentes/Experimento.tsx
+++ src/componentes/Experimento.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import {RLvsRL} from "../experimentos/RLvsRL";
+import {MiniMaxVsAlfaBeta} from "../experimentos/MiniMaxVsAlfaBeta";
 
 export const Experimento = () => {
 
     return (
-        <button onClick={() => RLvsRL()}>RLvsRL</button>
+        <>
+            <button onClick={() => RLvsRL()}>RLvsRL</button>
+            <button onClick={() => MiniMaxVsAlfaBeta(100, 3, 3)}>
+                Minimax(3)vsAlfaBeta(3)
+            </button>
+            <button onClick={() => MiniMaxVsAlfaBeta(100, 4, 2)}>
+                Minimax(4)vsAlfaBeta(2)
+            </button>
+            <button onClick={() => MiniMaxVsAlfaBeta(100, 2, 4)}>
+                Minimax(2)vsAlfaBeta(4)
+            </button>
+        </>
     )
 }
